f89aa4dbced229bbd858a8ffce78b1fe0160ece3
gogo <376830949@qq.com>
2016-02-29 23:27:18 +0800

    添加样式模块化mixin.scss，添加BaseTable组件

--- components/table/Table.js
+++ components/table/Table.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import BaseTable from '../common/BaseTable';
+import './table.scss';
 
 class Table extends Component {
 	render() {
 		return (
-			<div className="table">table</div>
+			<div className="table">
+				<Row>
+					<Col span="12">
+						<div className="box">
+					      	<BaseTable />
+					    </div>
+				    </Col>
+			    </Row>
+		    </div>
 	    );
 	}
 }
