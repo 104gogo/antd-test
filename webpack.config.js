1b738296ab504b27a65f57e20a1b93735826c86d
gogo <376830949@qq.com>
2016-03-07 19:38:38 +0800

    添加图片处理loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,12 @@ export default {
             { 
                 test: /\.scss$/, 
                 loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass')
-            }
-            // {
-            //      test: /.*\.(png|gif|svg|jpe?g)$/,
-            //      loader: 'file?name=../../images/[hash:8].[name].[ext]',
-            //      include: path.join(__dirname, "..",  "public")
-            // }
+            },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
         ]
     },
     resolve: {
