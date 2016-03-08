b7fe99fe6050e3b955da76848f4a72674040d624
gogo <376830949@qq.com>
2016-03-08 22:35:48 +0800

    修改store创建文件，为react-devtools加入做准备

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,11 @@ import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-	const store = createStore(
-		rootReducer,
-		initialState,
+	const finalCreateStore = compose(
 		applyMiddleware(thunk, createLogger())
-	);
+	)(createStore);
+
+	const store = finalCreateStore(rootReducer, initialState);
 
 	if(module.hot) {
 	    // Enable Webpack hot module replacement for reducers
